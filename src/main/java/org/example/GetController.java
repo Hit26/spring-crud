8431fadcd800dc850338b99b13797a523b143ac0
hitesh.jaware <hitesh.jaware@idfcfirstbank.com>
2023-12-16 18:51:26 +0530

    Changing Return type of GetController to ResponseEntity<> and adding test for it
    Returning Http Status from controller using ResponseEntity<> and writing test for it

--- src/main/java/org/example/GetController.java
+++ src/main/java/org/example/GetController.java
@@ -1,12 +1,13 @@
 package org.example;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //RestController is used for making restful web services with the help of the @RestController annotation. @restcontroller combination of the @controller and @ResponseBody annotation.
 public class GetController {
     @GetMapping("details") //It maps HTTP GET requests to a specific handler method in Spring controllers.
-    public String details() {
-        return "details";
+    public ResponseEntity<String> details() { //ResponseEntity<> : Extension of HttpEntity that adds an HttpStatusCode status code
+        return ResponseEntity.ok("details");
     }
 }
--- src/test/java/org/example/GetControllerTest.java
+++ src/test/java/org/example/GetControllerTest.java
@@ -5,16 +5,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
-@WebMvcTest(GetController.class) //testing annotation provided by the Spring Framework that focuses specifically on testing the web layer of a Spring MVC application
-@AutoConfigureMockMvc //Annotation that can be applied to a test class to enable and configure auto-configuration of MockMvc.
+@WebMvcTest(GetController.class)
+//testing annotation provided by the Spring Framework that focuses specifically on testing the web layer of a Spring MVC application
+@AutoConfigureMockMvc
+//Annotation that can be applied to a test class to enable and configure auto-configuration of MockMvc.
 public class GetControllerTest {
 
     @Autowired
     MockMvc mockMvc; //To test the web layer, we need MockMvc and by using @AutoConfigureMockMvc, we can write tests that will get injected.
 
     @Test
-    void shouldGetNoPersonDetailsIfNotPresent() {
+    void shouldGetNoPersonDetailsIfNotPresent() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/details")) //mockMvc.perform is used to perform requests and verify responses.
+                .andExpect(MockMvcResultMatchers.status().isOk())       //andExpect Performs an expectation.
+                .andExpect(content().string("details"));
     }
 }
